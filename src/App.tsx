9bba470ef4b6e4fffdbf1f3412a1d1a8c42bd1b1
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-12-06 12:58:44 +0530

    shadcn: Added all the components of shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HelmetProvider } from "react-helmet-async";
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient();
 
+const App = () => {
   return (
-    <>
-     
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
+    </HelmetProvider>
+  );
+};
 
-export default App
+export default App;
